18ae932df7d1b6d7d59762d2f943e5f90b6bf7f0
Zain Ul-Haq <zulhaq555@gmail.com>
2020-03-03 14:24:12 +0000

    fixes

--- Hoppers.java
+++ Hoppers.java
@@ -2,7 +2,7 @@
 /**Main Driver of Game Hoppers */
 public class Hoppers
 {
-    public static void main(String[] Args)
+   public static void main(String[] Args)
     {
         Board board = new Board();
     }

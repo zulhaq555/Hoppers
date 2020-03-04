5f58e6385e4efa2975f14a5e13d365d3572f3a16
zulhaq555 <60651435+zulhaq555@users.noreply.github.com>
2020-03-04 23:15:16 +0000

    Restart Project

--- Board.java
+++ Board.java
@@ -1,27 +1,12 @@
 /**This is the game interface (board) */
-
 import javax.swing.*;
 import  java.awt.*;
 
 public class Board
 {
-    JFrame a = new JFrame();
-    JPanel panel = new JPanel();
-    GridLayout g = new GridLayout(5,5);
-    ImageIcon water = new ImageIcon("Water.png");
-    JButton w = new JButton(water);
-    
-   
+      
     public Board()
     {
-        w.setIcon(water);
-        w.setPreferredSize(new Dimension(40,40));
-        panel.add(w);
-        a.setContentPane(panel);
-        panel.setLayout(g);
-        a.setTitle("Hoppers!");
-        a.setSize(1000,1000);
-        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        a.setVisible(true);
+        
     }
 }
--- Square.java
+++ Square.java
@@ -1,18 +1,11 @@
 /**square buttons and lilly pads */
-
 import javax.swing.*;
-
 
 public class Square 
 {
     
-    
-    
     public Square()
     {
-        ImageIcon water = new ImageIcon("Water.png");
-        JButton w = new JButton(water);
-        w.setIcon(water);
-        w.setSize(40,40);
+        
     }
 }
